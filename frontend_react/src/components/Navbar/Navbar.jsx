bd9ea1f0d31fe0b3d9628ebfd0d6b75052753277
FrankieZzzz <zhongweini0703@gmail.com>
2023-12-30 12:26:11 -0500

    finish nav bar menu design
    use framer motion and font awesome

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Navbar.scss'
 import { images } from '../../constants';
+import { FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import {motion} from 'framer-motion';
 
 
 const Navbar = () => {
-  
+  const [toggle, setToggle] = useState(false)
   return (
     <nav className='app__navbar'>
       <div className='app__navbar-logo'>
@@ -14,10 +17,29 @@ const Navbar = () => {
         {['About','Work','Experience','Contact'].map((item) => (
           <li key={`link-${item}`} className='app__flex p-text'>
             <div />
-            <a href={`#$item`}>{item}</a>
+            <a href={`#${item}`}>{item}</a>
           </li>
         ))}
       </ul>
+      {/* side menu on mobile */}
+      <div className='app__navbar-menu'>
+          <FontAwesomeIcon icon={faBars} onClick={() => setToggle(true)} />
+
+          {toggle && (
+            <motion.div
+              whileInView={{x: [-300, 0]}}
+              transition={{duration: 0.45, ease:'easeOut'}}>
+            <FontAwesomeIcon icon={faXmark} onClick={() => setToggle(false)} />
+            <ul>
+              {['About','Work','Experience','Contact'].map((item) => (
+                <li key={item}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
+                </li>
+              ))}
+            </ul>
+            </motion.div>
+          )}
+      </div>
     </nav>
   )
 }
