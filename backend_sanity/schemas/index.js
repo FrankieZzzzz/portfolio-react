5e60931bd6f461ddb67f0fbe1286453046239c07
FrankieZzzz <zhongweini0703@gmail.com>
2024-01-06 10:19:09 -0500

    ready to deploy

--- backend_sanity/schemas/index.js
+++ backend_sanity/schemas/index.js
@@ -6,4 +6,5 @@ import bgImg from'./bgImg';
 import skills from'./skills';
 import workExperience from'./workExperience';
 import experiences from'./experiences';
-export const schemaTypes = [testimonials, abouts, works, bgImg, skills, workExperience, experiences];
+import contact from'./contact';
+export const schemaTypes = [abouts, works, skills, workExperience, experiences, contact];
